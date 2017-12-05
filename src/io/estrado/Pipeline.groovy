42b2e1d374b93424d73c2dc6afff4096b0358a20
David Classen <david@adhoclabs.co>
2017-12-05 15:01:00 -0500

    BUR-1442 - add sbt 1.1.0-RC1 support

--- src/io/estrado/Pipeline.groovy
+++ src/io/estrado/Pipeline.groovy
@@ -28,11 +28,15 @@ def genNexusRepoConf() {
         mkdir -p .sbt/0.13/plugins
         mkdir -p .sbt/1.0/plugins
         mkdir -p .sbt/1.1.0-RC1/plugins
+        mkdir -p .sbt/1.1.0/plugins
+        mkdir -p .sbt/1/plugins
         echo  \"${sbt_repos}\"  > .sbt/repositories
         echo  \"${sbt_creds}\" > .sbt/.credentials
         echo  \"${sbt_creds_plugin}\" > .sbt/0.13/plugins/plugins.sbt
         echo  \"${sbt_creds_plugin}\" > .sbt/1.0/plugins/plugins.sbt
         echo  \"${sbt_creds_plugin}\" > .sbt/1.1.0-RC1/plugins/plugins.sbt
+        echo  \"${sbt_creds_plugin}\" > .sbt/1.1.0/plugins/plugins.sbt
+        echo  \"${sbt_creds_plugin}\" > .sbt/1/plugins/plugins.sbt
       """
 }
 
